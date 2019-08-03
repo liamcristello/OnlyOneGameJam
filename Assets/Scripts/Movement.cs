96dc2af93dc212245b4ff020c0939d2ed7c6454a
Liam Cristello <liamcristello@gmail.com>
2019-08-03 18:21:09 -0400

    Basic Text Display
    Player can engage dialogue when within trigger area for an object. Remains to be seen if multiple objects can coexist without breaking everything.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,24 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+// Script to manage player movement.
+public class Movement : MonoBehaviour
 {
+    // float to represent the upper bound of the player's speed.
     public float maxWalkSpeed;
+    // Vector3 to represent how fast the player is currently moving.
     Vector3 playerVelocity;
-    Rigidbody2D rb2d;
+    // Rigidbody2D to determine physics input on player.
+    private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Start playerVelocity at 0.
         playerVelocity = Vector3.zero;
+        // Locate the player's rigidbody2D component for the rb2d variable.
         rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Take in x-direction input and affect the player's x-direction speed through rb2d.
         playerVelocity.x = Input.GetAxis("Horizontal") * maxWalkSpeed; 
-    
         rb2d.velocity = playerVelocity;
     }
 }
--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Script to manage objects' interactable trigger areas.
 public class ObjectManager : MonoBehaviour
 {
+    // Reference to GameObject for purposes of displaying dialogue/text
+    public DisplayText displayText;
+
+    // bool to track if player is in collider trigger to start dialogue/text
+    public static bool inZone;
+
     // Start is called before the first frame update
     void Start()
      {
-         
+         inZone = false;
      }
  
     // Update is called once per frame
@@ -16,19 +23,29 @@ public class ObjectManager : MonoBehaviour
          
      }
 
+    // Called when something enters this object's box-collider trigger.
     void OnTriggerEnter2D (Collider2D other) {
+        // If the Collider2D belongs to the player
          if (other.gameObject.CompareTag("Player")) {
              Debug.Log ("Player has entered interactable zone.");
+             inZone = true;
          }
      }
-     void OnTriggerStay2D (Collider2D other) {
-         if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E)) {
-             Debug.Log ("Player has interacted with " + this.name + ".");
-         }
-     }
+     
+     // Called when something remains in this object's box-collider trigger.
+    //  void OnTriggerStay2D (Collider2D other) {
+    //      // If the Collider2D belongs to the player
+    //      if (other.gameObject.CompareTag("Player") && Input.GetKeyDown(KeyCode.E)) {
+    //          Debug.Log ("Player has interacted with " + this.name + ".");
+    //      }
+    //  }
+
+     // Called when something exits this object's box-collider trigger.
      void OnTriggerExit2D (Collider2D other) {
+         // If the Collider2D belongs to the player
          if (other.gameObject.CompareTag("Player")) {
              Debug.Log ("Player has exited interactable zone.");
+             inZone = false;
          }
      }
 }
