6e79f46aa792d1cd323f29545544bdd5fefc0ef0
Liam Cristello <liamcristello@gmail.com>
2019-08-02 21:58:49 -0400

    Basic Walking
    Character should be able to walk with this script. Stairs may be a challenge.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float maxWalkSpeed;
+    Vector3 playerVelocity;
+    Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerVelocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerVelocity.x = Input.GetAxis("Horizontal") * maxWalkSpeed; 
+    
+        rb2d.velocity = playerVelocity;
+    }
+}
